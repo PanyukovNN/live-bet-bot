c4f5940b34065cb732eaa96c16b06ebf8753bb8b
Nickolay <zylex77@gmail.com>
2020-01-06 23:35:23 +0300

    add ScheduledResultScanningTask

---
+++ src/main/java/com/zylex/livebetbot/ScheduledResultScanningTask.java
@@ -0,0 +1,29 @@
+package com.zylex.livebetbot;
+
+import com.zylex.livebetbot.controller.dao.GameDao;
+import com.zylex.livebetbot.service.ResultScanner;
+import org.openqa.selenium.WebDriver;
+
+public class ScheduledResultScanningTask implements Runnable {
+
+    private GameDao gameDao;
+
+    private WebDriver driver;
+
+    ScheduledResultScanningTask(GameDao gameDao, WebDriver driver) {
+        this.gameDao = gameDao;
+        this.driver = driver;
+    }
+
+    @Override
+    public void run() {
+        try {
+            new ResultScanner(
+                driver,
+                gameDao
+            ).scan();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
